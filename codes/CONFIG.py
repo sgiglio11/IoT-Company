e5958f1e489be9dbcbf1fa71b889309b8b8af11e
sgiglio11 <s.giglio9@studenti.unisa.it>
2022-07-24 16:16:29 +0200

    Initial commit

--- codes/CONFIG.py
+++ codes/CONFIG.py
@@ -2,5 +2,5 @@ import os
 
 class DefaultConfig:
     FLASK_KEY = os.environ.get("FlaskKey", "")
-    EMAIL_MANAGER = os.environ.get("EmailKey1", "")
+    EMAIL_MANAGER = os.environ.get("EmailKey", "")
     EMAIL_PROTOTYPE = os.environ.get("EmailKey2", "")
